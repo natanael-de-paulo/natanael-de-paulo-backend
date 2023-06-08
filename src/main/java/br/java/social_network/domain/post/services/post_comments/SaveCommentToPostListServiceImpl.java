d8f213327306df41cee6206082134b0d31a0ed85
Natanael de Paulo <natanaeldepaulo123@gmail.com>
2023-06-08 10:48:49 -0300

    refact e feat: insert api error message and refact services

--- src/main/java/br/java/social_network/domain/post/services/post_comments/SaveCommentToPostListServiceImpl.java
+++ src/main/java/br/java/social_network/domain/post/services/post_comments/SaveCommentToPostListServiceImpl.java
@@ -2,10 +2,13 @@ package br.java.social_network.domain.post.services.post_comments;
 
 import br.java.social_network.application.post.services.IPostService;
 import br.java.social_network.domain.post.embedded.Comment;
+import br.java.social_network.infrastructure.exception.HandleNotFoundException;
 import br.java.social_network.infrastructure.repositories.IPostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+
+import java.util.Objects;
 
 @Service
 @Qualifier("SaveCommentToPostListServiceImpl")
@@ -15,9 +18,17 @@ public class SaveCommentToPostListServiceImpl implements IPostService<Comment, V
 
     @Override
     public Void execute(Comment comment) {
-        var post = this.postRepository.findById(comment.getPost_id());
-        post.get().getComments().add(comment);
-        this.postRepository.save(post.get());
-        return null;
+        try {
+            var post = this.postRepository.findById(comment.getPost_id());
+
+            post.get().getComments().add(comment);
+
+            this.postRepository.save(post.get());
+
+            return null;
+        } catch (Exception e){
+            if (e.getMessage() == "No value present") throw new HandleNotFoundException("Post not found");
+            throw new HandleNotFoundException(e.getMessage());
+        }
     }
 }
--- src/main/java/br/java/social_network/domain/user/services/FollowUserServiceImpl.java
+++ src/main/java/br/java/social_network/domain/user/services/FollowUserServiceImpl.java
@@ -3,6 +3,7 @@ package br.java.social_network.domain.user.services;
 import br.java.social_network.application.user.services.IUserService;
 import br.java.social_network.application.utils.ConvertFormatId;
 import br.java.social_network.domain.user.User;
+import br.java.social_network.infrastructure.exception.HandleNotFoundException;
 import br.java.social_network.infrastructure.repositories.IUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -10,6 +11,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
+import java.util.Objects;
 import java.util.UUID;
 
 @Service
@@ -20,24 +22,28 @@ public class FollowUserServiceImpl implements IUserService<String, String> {
 
     @Override
     public String execute(String userId) {
-        var userLogged = ((User) SecurityContextHolder.getContext().getAuthentication().getPrincipal());
-        var userLoggedEntity = this.userRepository.findById(userLogged.getId());
-        var handleUser = this.userRepository.findById(ConvertFormatId.toUUID(userId));
+      try{
+          var userLogged = ((User) SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+          var userLoggedEntity = this.userRepository.findById(userLogged.getId());
+          var handleUser = this.userRepository.findById(ConvertFormatId.toUUID(userId));
 
-        if (handleUser.isEmpty()) throw new RuntimeException("User not found");
+          if (Objects.isNull(handleUser)) throw new Exception("User not found");
 
 
-        var listFollowersUserLoggedEntity = userLoggedEntity.get().getProfile().getFollowing();
-        var userLoggedId = userLoggedEntity.get().getId();
-        var handleUserId = handleUser.get().getId();
+          var listFollowersUserLoggedEntity = userLoggedEntity.get().getProfile().getFollowing();
+          var userLoggedId = userLoggedEntity.get().getId();
+          var handleUserId = handleUser.get().getId();
 
-        if (!listFollowersUserLoggedEntity.contains(handleUserId)) {
-            addFollower(handleUserId, userLoggedId, handleUser.get(), userLoggedEntity.get());
-            return "You follow";
-        }
+          if (!listFollowersUserLoggedEntity.contains(handleUserId)) {
+              addFollower(handleUserId, userLoggedId, handleUser.get(), userLoggedEntity.get());
+              return "You follow";
+          }
 
-        removeFollower(handleUserId, userLoggedId, handleUser.get(), userLoggedEntity.get());
-        return "You unfollowed";
+          removeFollower(handleUserId, userLoggedId, handleUser.get(), userLoggedEntity.get());
+          return "You unfollowed";
+      } catch (Exception e) {
+          throw new HandleNotFoundException(e.getMessage());
+      }
     }
 
     private void removeFollower(UUID handleUserId, UUID userLoggedId, User handleUserEntity, User userLoggedEntity) {
